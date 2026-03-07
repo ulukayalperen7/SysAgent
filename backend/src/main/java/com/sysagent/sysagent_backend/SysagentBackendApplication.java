a356cbd935a0348b0e433726ae79de37e5e3214d
Alperen Ulukaya <alperenulukaya07@gmail.com>
2026-03-07 11:06:16 +0300

    feat(backend): setup Spring Boot core with H2 and JPA configs

---
+++ backend/src/main/java/com/sysagent/sysagent_backend/SysagentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sysagent.sysagent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysagentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysagentBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sysagent/sysagent_backend/SysagentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysagent.sysagent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysagentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
